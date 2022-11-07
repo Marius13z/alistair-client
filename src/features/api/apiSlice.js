55211a13b366f78d7d1ddb8718a116c30472bafb
Marius13z <lemonlcsnet@yahoo.com>
2022-11-07 20:05:21 +0200

    url-change

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://127.0.0.1:5000",
+    baseUrl: "https://alistair-server.herokuapp.com/",
     credentials: "include",
     prepareHeaders: (headers) => {
       return headers;
